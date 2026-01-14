7dc6ef17ca73437cfb71da4772438775781e8400
Soeurn-Visal <visalsoeurn9@gmail.com>
2026-01-15 02:36:01 +0700

    modify config repo

--- order-service/src/main/java/com/see/order/order_service/exection/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package com.see.order.order_service.exection;
-
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
